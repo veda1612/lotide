11220034f2e42d904bd4838dc992b5e57a2f7d10
veda1612 <vedavasavisiri@gmail.com>
2021-01-06 01:21:31 +0000

    assertArraysEqual added

---
+++ assertArraysEqual.js
@@ -0,0 +1,36 @@
+//copied eqArrays function
+const eqArrays = function(array1, array2) {
+    let correct = true;
+  
+//if the two arrays are not the same length the assertion fails automatically
+    if (array1.length !== array2.length) {
+      correct = false;
+    } 
+  
+//if the two arrays are the same length this runs:
+    for (let i = 0; i < array1.length; i ++) {
+      if (array1[i] !== array2[i]) {
+        correct = false;
+      }
+    }
+  
+    return correct;
+  
+}
+const assertArraysEqual = function(arr1, arr2) {
+
+    if (eqArrays(arr1, arr2)) {  //if the true (two arrays are equal)
+      console.log(`😜😍🍆Arrays are equal!`)
+    } else {
+      console.log(`😡🤮🤢Arrays are NOT equal!`)
+    }
+  
+  }
+  
+  
+  //test cases
+//assertArraysEqual(["hey", "hi", "hello"], ["hey", "hi", "hello"]);
+assertArraysEqual(["hey", "hi", "bye"], ["hey", "hi", "hello"]);
+
+  
+module.exports = assertArraysEqual;
