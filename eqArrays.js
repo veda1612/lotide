eb23e369e5536ab14a431d09c187287c327fe857
veda1612 <vedavasavisiri@gmail.com>
2021-01-05 22:30:23 -0500

    Update eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,9 @@
 //copied assertEqual function
 const assertEqual = function(actual, expected) {
     if (actual !== expected) {
-      console.log(`😡🤮🤢Assertion Failed: ${actual} !== ${expected}`);
+      console.log(`Assertion Failed: ${actual} !== ${expected}`);
     } else {
-      console.log(`😜😍🍆Assertion Passed: ${actual} === ${expected}`);
+      console.log(`Assertion Passed: ${actual} === ${expected}`);
     }
   };
 //eqArrays function
