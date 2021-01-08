52ff699d49dd77007159345ff5383d02e33c651c
veda1612 <vedavasavisiri@gmail.com>
2021-01-08 03:41:54 +0000

    added map.js

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -4,11 +4,13 @@ const eqObjects = require("./eqObjects");
 
 // FUNCTION IMPLEMENTATION
 const assertObjectsEqual = function(actual, expected) {
-    const inspect = require('util').inspect; 
+    //const inspect = require('util').inspect; 
     if (!eqObjects(actual, expected)){
-        return (console.log(`❌❌❌ Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`));
+        //return (console.log(`❌❌❌ Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`));
+        return (console.log(`❌❌❌ Assertion Failed: `));
     }
-    return (console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`));
+    //return (console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`));
+    return (console.log(`✅✅✅ Assertion Passed: `));
 };
 
 //test cases
---
+++ map.js
@@ -0,0 +1,15 @@
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array, callback) {
+    const results = [];
+    for (let item of array) {
+        //console.log('item BEFORE: ', item);
+        //console.log('item AFTER: ', callback(item));
+        //console.log('---');
+        results.push(callback(item));
+      }
+    return results;
+  }
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+module.exports = map;
