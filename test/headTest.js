7b65eed9f9ff500dd9d0fbf304598fa1e501e7f6
veda1612 <vedavasavisiri@gmail.com>
2021-01-11 21:50:46 +0000

    all done

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,14 @@
 
 const head= require('../head');
 const assertEqual = require('../assertEqual');
+const assert = require('assert');
 
 //test cases
-assertEqual(head([5,6,7]), 6);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+//assertEqual(head([5,6,7]), 6);
+//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+describe("#head", () => {
+    it("returns '5' for ['5']", () => {
+     assert.strictEqual(head(['5']), '5'); 
+  });
+});
--- test/middleTest.js
+++ test/middleTest.js
@@ -5,5 +5,11 @@ const assertArraysEqual = require('../assertArraysEqual');
 //test cases
 //console.log(middle([1,2,3]));
 //console.log(middle([1,2,3,4,5,6]));
-assertArraysEqual(middle([1,2,3], [2]));
-assertArraysEqual(middle([1,2,3,4,5,6], [3,4]));
+//assertArraysEqual(middle([1,2,3], [2]));
+//assertArraysEqual(middle([1,2,3,4,5,6], [3,4]));
+
+describe("#middle", () => {
+    it("returns '2' for ['2']", () => {
+      assert.deepEqual(middle(['2']), '2'); 
+    });
+  });
